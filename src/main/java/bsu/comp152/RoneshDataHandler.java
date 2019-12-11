6c6eced7a38a2241968c9a4713ba9726f4e99e14
spillthesmoothie <54909115+spillthesmoothie@users.noreply.github.com>
2019-12-11 16:36:34 -0500

    more methods.

--- src/main/java/bsu/comp152/RoneshDataHandler.java
+++ src/main/java/bsu/comp152/RoneshDataHandler.java
@@ -8,15 +8,19 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class RoneshDataHandler {
 
     private HttpClient dataGrabber;
     private String webLocation;
+    private String webLocation1;
 
     public RoneshDataHandler (String webLocation){
         dataGrabber = HttpClient.newHttpClient();
      this.webLocation = webLocation;}
+
+
      public ArrayList<weatherData> getData(){
     var requestBuilder = HttpRequest.newBuilder();
     var dataRequest = requestBuilder.uri(
@@ -46,13 +50,79 @@ public class RoneshDataHandler {
     var usefulData = response.body();
     var jsonInterpreter =new Gson();
     var weatherData =jsonInterpreter.fromJson(usefulData,responseDataType.class);
-        return weatherData.results;
+        return weatherData.consolidated_weather;
 }
+    public int getData1(){
+        var requestBuilder = HttpRequest.newBuilder();
+        var dataRequest = requestBuilder.uri(
+                URI.create(webLocation)).build();
+        HttpResponse<String> response = null;
+        try
+        {
+            response = dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
+        }catch(
+                IOException e)
+        {
+            System.out.println("Error connecting to network or site");
+        }
+        catch(
+                InterruptedException e)
+
+        {
+            System.out.println("Connection to site broken");
+        }
+        if(response ==null)
+
+        {
+            System.out.println("Something went terribly wrong, ending program");
+            System.exit(-1);
+        }
+        System.out.println(response);
+        var usefulData = response.body();
+        var jsonInterpreter =new Gson();
+        var weatherData =jsonInterpreter.fromJson(usefulData,responseDataType1.class);
+        return weatherData.woeid;
+    }
+
+class responseDataType1{
+        String title;
+        String location_time;
+        int woeid;
+
+    @Override
+    public String toString() {
+        return "responseDataType1{" +
+                "title='" + title + '\'' +
+                ", location_time='" + location_time + '\'' +
+                ", woeid=" + woeid +
+                '}';
+    }
+
+    public responseDataType1(String title, String location_time, int woeid) {
+        this.title = title;
+        this.location_time = location_time;
+        this.woeid = woeid;
+    }
+}
+
 class responseDataType{
-        ArrayList<weatherData> results;
+        ArrayList<weatherData> consolidated_weather;
 }
 class weatherData{
         String id;
+        Date applicable_date;
+        String weather_state_name;
+        String weather_state_abbr;
+        float wind_speed;
+        float wind_direction;
+        int min_temp;
+        int max_temp;
+        int the_temp;
+        float air_pressure;
+        float humidity;
+        float visibility;
+        float predictability;
+
 }
 }
 
--- src/main/java/bsu/comp152/RoneshWeatherController.java
+++ src/main/java/bsu/comp152/RoneshWeatherController.java
@@ -4,10 +4,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class RoneshWeatherController {
     @FXML
@@ -25,11 +29,26 @@ public class RoneshWeatherController {
     private TextArea citynametext;
     @FXML
     private TextArea woeidtext;
+    @FXML
+    private ListView listControl;
+
+
+
+    public void loadData1(String cityname){
+        var site = "https://www.metaweather.com/api/location/search/?query=";
+        var query = site+cityname;
+        Model = new RoneshDataHandler(query);
+        var weatherList = Model.getData();
+        ObservableList<RoneshDataHandler.weatherData> dataToShow =
+                FXCollections.observableArrayList(weatherList);
+        ListControl.setItems(dataToShow);
+
+    }
 
 
 
     public void loadData(){
-        var site = "https://www.metaweather.com/api/";
+        var site = "https://www.metaweather.com/api/location/";
         var params = getQueryParameters();
         var query = site+params;
         Model = new RoneshDataHandler(query);
@@ -57,15 +76,44 @@ public class RoneshWeatherController {
 
     private  String getdate(){
         datepicker.setEditable(true);
-        
+        var getthedate = datepicker.getAccessibleText();
+        return getthedate;
     }
+
+
+    private void showData(){
+
+    }
+
+
+
     public String getQueryParameters(){
+        var getDate = getdate();
+        var getWoeid = getWoeid();
+        String param = "";
+        if (getWoeid.isBlank()){
+            System.out.println("No Woeid Selected, Search for city's woeid");
+            loadData1(getCity());
 
+        }else {
+            param = getWoeid;
+            loadData();
+        }
 
-
-
-    }
+        return param;
+            }
     @FXML
     public void exit(ActionEvent event){
         System.exit(0);//will exit the program
+}
+    @FXML
+    public void search(ActionEvent event){
+        loadData1(getCity());
+        listControl.getItems();
+
+
+
+
+
 }}
+
