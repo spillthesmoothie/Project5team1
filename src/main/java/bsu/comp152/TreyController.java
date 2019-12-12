c758c9432d0fe0f9519821062bafb944024ca8c7
blackington <54909118+blackington@users.noreply.github.com>
2019-12-12 14:18:25 -0500

    Update TreyController.java

--- src/main/java/bsu/comp152/TreyController.java
+++ src/main/java/bsu/comp152/TreyController.java
@@ -1,4 +1,50 @@
 package bsu.comp152;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class TreyController {
+
+    @FXML
+    private Button Programming;
+    @FXML
+    private Button Dad;
+    @FXML
+    private Button Any;
+
+    @FXML
+    private void dadJoke(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("jokeScreen.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    @FXML
+    private void progJoke(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("jokeScreen.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+
+    @FXML
+    private void anyJoke(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("jokeScreen.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+    
+
 }
+
